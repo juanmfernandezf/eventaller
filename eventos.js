89e2fa2788d5c779887c991b70da61c890569eb7
Juan Fernandez <juanma141717@gmail.com>
2023-08-15 02:15:39 -0300

    eventos js

---
+++ eventos.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("saludarButton");
+
+    button.addEventListener("click", function() {
+        alert("¡Hola!");
+    });
+
+    var div = document.getElementById("botonDiv");
+
+    div.addEventListener("click", function() {
+        alert("¡Hola! Soy el div");
+    });
+});
